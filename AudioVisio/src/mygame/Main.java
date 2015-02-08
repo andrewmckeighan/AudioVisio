ad6fd9c7bf5562e0e26edcd5ca532a54da195f2a
tpremo <tpremo@iastate.edu>
2015-02-08 03:08:02 -0600

    Added more code to the Main.java, cube now rotates & 'p' pauses.

--- AudioVisio/src/mygame/Main.java
+++ AudioVisio/src/mygame/Main.java
@@ -1,6 +1,8 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
@@ -9,9 +11,9 @@ import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 
 /**
- * test
- * @author normenhansen
- */
+* test
+* @author normenhansen
+*/
 public class Main extends SimpleApplication {
 
     public static void main(String[] args) {
@@ -19,25 +21,45 @@ public class Main extends SimpleApplication {
         app.start();
     }
 
+    protected Geometry player;
+    Boolean isRunning = true;
+
     @Override
     public void simpleInitApp() {
         Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
+        player = new Geometry("Box", b);
 
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
+        player.setMaterial(mat);
 
-        rootNode.attachChild(geom);
+        rootNode.attachChild(player);
+        initKeys();
     }
+
+    private void initKeys() {
+        inputManager.addMapping("Pause", new KeyTrigger(keyInput.KEY_P));
+
+        inputManager.addListener(actionListener, "Pause");
+    }
+
+    private ActionListener actionListener = new ActionListener() {
+        public void onAction(String name, boolean keyPressed, float tpf) {
+            if(name.equals("Pause") && !keyPressed){
+                isRunning = !isRunning;
+            }
+        }
+    };
 
     @Override
     public void simpleUpdate(float tpf) {
-        //TODO: add update code
+        if(isRunning){
+            player.rotate(3*tpf,2*tpf, 1*tpf);
+        }
     }
 
     @Override
     public void simpleRender(RenderManager rm) {
-        //TODO: add render code
+//TODO: add render code
     }
 }
