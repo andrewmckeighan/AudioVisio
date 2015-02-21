277bcfc81eb6b0baf323136798f7e2f6e4d20d4c
jamesl <jamesl@iastate.edu>
2015-02-21 14:31:05 -0600

    refactored networking files, main

--- AudioVisio/src/audiovisio/AudioVisio.java
+++ AudioVisio/src/audiovisio/AudioVisio.java
@@ -1,9 +1,18 @@
 package audiovisio;
 
+import audiovisio.networking.Server;
+import audiovisio.networking.Client;
+import com.jme3.system.JmeContext;
+
 public class AudioVisio {
 
     public static void main(String[] args) {
-
+    	//Server Start
+    	audiovisio.networking.Server serverApp = new Server(null, null);
+    	clientApp.start(JmeContext.Type.Headless);
+    	//
+    	Client clientApp = new Client();
+		clientApp.start();
     }
 
 }
