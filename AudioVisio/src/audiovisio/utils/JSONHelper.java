73acd7084129dcb08626046107b3d0eecef53e12
mjgerst <mjgerst@iastate.edu>
2015-02-22 17:55:29 -0600

    Add Javadoc to some level classes and the JSONHelper

--- AudioVisio/src/audiovisio/utils/JSONHelper.java
+++ AudioVisio/src/audiovisio/utils/JSONHelper.java
@@ -4,20 +4,57 @@ import org.json.simple.JSONObject;
 
 import com.jme3.math.Vector3f;
 
+/**
+ * A collection of methods to help with reading
+ * and writing common data structures to JSON
+ */
 public class JSONHelper {
-	public static Vector3f readVector3f(JSONObject location) {
-		float locX = ((Double) location.get("x")).floatValue();
-		float locY = ((Double) location.get("y")).floatValue();
-		float locZ = ((Double) location.get("z")).floatValue();
+	/**
+	 * Read a Vector3f from a JSONObject.
+	 * 
+	 * The JSON object should be of the form:
+	 * <pre>
+	 * {
+	 *    "x": 2,
+	 *    "y": 3,
+	 *    "z": 1
+	 * }
+	 * </pre>
+	 * 
+	 * The JSONObject passed to this function should be the
+	 * object obtained from using <code>obj.get("location")</code>
+	 * 
+	 * @param obj The json object to read
+	 * @return The Vector3f created from the JSONObject
+	 */
+	public static Vector3f readVector3f(JSONObject obj) {
+		float locX = ((Double) obj.get("x")).floatValue();
+		float locY = ((Double) obj.get("y")).floatValue();
+		float locZ = ((Double) obj.get("z")).floatValue();
 		
 		return new Vector3f(locX, locY, locZ);
 	}
 	
-	public static JSONObject saveVector3f(Vector3f location) {
+	/**
+	 * Save a Vector3f to a JSONObject.
+	 * 
+	 * The JSON object that is returned is:
+	 * <pre>
+	 * {
+	 *    "x": 2,
+	 *    "y": 3,
+	 *    "z": 1
+	 * }
+	 * </pre>
+	 * 
+	 * @param vec The Vector3f to save to a json Object
+	 * @return The JSONObject created from the Vector3f
+	 */
+	public static JSONObject saveVector3f(Vector3f vec) {
 		JSONObject loc = new JSONObject();
-		loc.put("x", location.x);
-		loc.put("y", location.y);
-		loc.put("z", location.z);
+		loc.put("x", vec.x);
+		loc.put("y", vec.y);
+		loc.put("z", vec.z);
 		
 		return loc;
 	}
