a797584941a27727cc1c4ad52938a1b60426990a
mjgerst <mjgerst@iastate.edu>
2015-02-21 20:07:51 -0600

    Restructure the load/save mechanism for the level.

--- AudioVisio/src/audiovisio/level/Panel.java
+++ AudioVisio/src/audiovisio/level/Panel.java
@@ -6,19 +6,20 @@ import com.jme3.math.Vector3f;
 
 public class Panel {
 	public Vector3f location;
+	
+	public Panel() {}
 
     public Panel(Vector3f location){
         this.location = location;
     }
     
-    public static Panel load(JSONObject obj) {
+    public void load(JSONObject obj) {
     	JSONObject location = (JSONObject) obj.get("location");
     	float locX = ((Double)location.get("x")).floatValue();
     	float locY = ((Double)location.get("y")).floatValue();
     	float locZ = ((Double)location.get("z")).floatValue();
     	
     	Vector3f locVec = new Vector3f(locX, locY, locZ);
-    	return new Panel(locVec);
     }
     
     public void save(JSONObject obj) {
