a1a295b717da273800096cc54a6c82cd75b0fc3d
amm <amm@iastate.edu>
2015-02-21 18:09:53 -0800

    lots of changes. Commented out the Gui.java to try out the nifty library. made a niftyTest.java to tryout the nifty library to work better with jMonkey3Engine

--- AudioVisio/src/audiovisio/gui/Main.java
+++ AudioVisio/src/audiovisio/gui/Main.java
@@ -3,8 +3,10 @@ package audiovisio.gui;
 public class Main {
 
   public static void main(String args[])  {
-	  Gui gui = new Gui();
-	  gui.start();
+//	  Gui gui = new Gui();
+//	  gui.start();
+	  niftyTest nifty = new niftyTest();
+	  nifty.start();
   }
 }
 
