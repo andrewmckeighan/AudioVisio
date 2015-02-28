7e0206099231e60b517bda3fde1f227afc80dd78
amm <amm@iastate.edu>
2015-02-28 17:05:51 -0600

    trying another method for making gui

--- AudioVisio/src/audiovisio/gui/Main.java
+++ AudioVisio/src/audiovisio/gui/Main.java
@@ -5,7 +5,7 @@ public class Main {
   public static void main(String args[])  {
 //	  Gui gui = new Gui();
 //	  gui.start();
-	  niftyTest nifty = new niftyTest();
+	  NewGui nifty = new NewGui();
 	  nifty.start();
   }
 }
