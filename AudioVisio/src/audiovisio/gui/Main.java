6a9ea5c02da984a0d3be43d2935e72d337cf15b3
amm <amm@iastate.edu>
2015-02-28 19:31:33 -0600

    updated xml

--- AudioVisio/src/audiovisio/gui/Main.java
+++ AudioVisio/src/audiovisio/gui/Main.java
@@ -1,32 +1,33 @@
 package audiovisio.gui;
 
-
 import com.jme3.app.SimpleApplication;
 import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.system.AppSettings;
-
 import de.lessvoid.nifty.Nifty;
 
 public class Main extends SimpleApplication {
+
 	private Nifty nifty;
 
 	public static void main(String args[]) {
 		// Gui gui = new Gui();
 		// gui.start();
 		AppSettings settings = new AppSettings(true);
-        settings.setAudioRenderer("LWJGL");
-        Main app = new Main();
-        app.setSettings(settings);
-        app.start();
+		settings.setAudioRenderer("LWJGL");
+		Main app = new Main();
+		app.setSettings(settings);
+		app.start();
 	}
-	
-	public void simpleInitApp(){
-		NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager, inputManager, 
-				audioRenderer, guiViewPort);
+
+	public void simpleInitApp() {
+		NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager,
+				inputManager, audioRenderer, guiViewPort);
 		nifty = niftyDisplay.getNifty();
 		nifty.fromXml("audiovisio/gui/baselayer.xml", "start");
+		guiViewPort.addProcessor(niftyDisplay);
+		flyCam.setEnabled(false);
 		inputManager.setCursorVisible(true);
 		
 	}
-	
+
 }
