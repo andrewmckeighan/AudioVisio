bd3c4518cb8bc5f8af0f557cdcc19e7853872283
mjgerst <mjgerst@iastate.edu>
2015-03-01 23:09:02 -0600

    Add physics sync messages.

---
+++ AudioVisio/src/audiovisio/networking/messages/SyncCharacterMessage.java
@@ -0,0 +1,34 @@
+package audiovisio.networking.messages;
+
+import com.jme3.bullet.control.BetterCharacterControl;
+import com.jme3.math.Vector3f;
+import com.jme3.network.serializing.Serializable;
+import com.jme3.scene.Spatial;
+
+@Serializable
+public class SyncCharacterMessage extends PhysicsSyncMessage {
+
+    public Vector3f location = new Vector3f();
+    public Vector3f walkDirection = new Vector3f();
+    public Vector3f viewDirection = new Vector3f();
+
+    public SyncCharacterMessage() {}
+
+    public SyncCharacterMessage(long id, BetterCharacterControl character) {
+        this.syncId = id;
+
+        this.walkDirection.set(character.getWalkDirection());
+        this.viewDirection.set(character.getViewDirection());
+    }
+
+    public void readData(BetterCharacterControl character) {
+        this.walkDirection.set(character.getWalkDirection());
+        this.viewDirection.set(character.getViewDirection());
+    }
+
+    public void applyData(Object character) {
+        ((Spatial) character).getControl(BetterCharacterControl.class).warp(location);
+        ((Spatial) character).getControl(BetterCharacterControl.class).setWalkDirection(walkDirection);
+        ((Spatial) character).getControl(BetterCharacterControl.class).setViewDirection(viewDirection);
+    }
+}
