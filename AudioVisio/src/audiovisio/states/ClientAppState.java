dc41f3f579556b91ab4e5956dd979b7191f5e2f2
tpremo <tpremo@iastate.edu>
2015-04-01 23:16:05 -0500

    rewriting particle

--- AudioVisio/src/audiovisio/states/ClientAppState.java
+++ AudioVisio/src/audiovisio/states/ClientAppState.java
@@ -146,7 +146,7 @@ public class ClientAppState extends AbstractAppState implements
         // Create Camera //
         // /////////////////
         this.audioVisioApp.getFlyByCamera().setEnabled(true);
-        this.audioVisioApp.getViewPort().setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f));
+        this.audioVisioApp.getViewPort().setBackgroundColor(new ColorRGBA(0.0f, 0.0f, 0.0f, 1.0f));
         this.audioVisioApp.getFlyByCamera().setMoveSpeed(100);
 
         // ////////////////
